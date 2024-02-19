5865dd9cca7c37cfc7b695a89ab40a7afa70024f
ABM-Radhekrishna <146554369+ABM-Radhekrishna@users.noreply.github.com>
2024-02-19 09:57:20 +0530

    Update EXP4.m

--- EXP4.m
+++ EXP4.m
@@ -8,14 +8,25 @@ A(1,1) = 2;
 A(Nx,Nx)=2;
 A(7,6)=-1;
 for i = 2:Nx-1
-    A(i,i-1) =-1
-    A(i,i) = 2
-    A(i,i+1) =-1
-    A(i-1,i) = -1
+    A(i,i-1) =-1;
+    A(i,i) = 2;
+    A(i,i+1) =-1;
+    A(i-1,i) = -1;
+    
 end
-disp(A)
-b = zeros(Nx,1);
-b(1) = 100;
-b(Nx) = 10;
 
-X = eig(A);
+b = zeros(Nx,1);
+x = linspace(0,L,Nx);
+[L,D] = eig(A);
+S = diag(D);
+
+I = eye(7);
+I(1,1) = S(1,1);
+I(7,7) = S(7,1);
+for i = 2:7
+    I(i,i) = S(i,1);
+end
+T = A./I;
+t = diag(T);
+disp(t)
+plot(x,t);
